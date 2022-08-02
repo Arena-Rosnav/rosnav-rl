cf3e646bf970ad34002a7ead668389dbbc6d61a2
cliebig2019 <82951475+cliebig2019@users.noreply.github.com>
2022-08-02 17:44:52 +0200

    added cnn and tanh to neural network modules

--- rosnav/model/custom_policy_utils/modules.py
+++ rosnav/model/custom_policy_utils/modules.py
@@ -6,7 +6,23 @@ def linear(params):
 def relu(params):
     return nn.ReLU(params.get("inplace", False))
 
+def conv1d(params):
+    return nn.Conv1d(params["in_channels"], params["out_channels"], params["kernel_size"], params.get["stride"], params.get["padding"], params.get["padding_mode"], params.get["dilation"], params.get["groups"], params.get["bias"])
+
+def conv2d(params):
+    return nn.Conv1d(params["in_channels"], params["out_channels"], params["kernel_size"], params.get["stride"], params.get["padding"], params.get["padding_mode"], params.get["dilation"], params.get["groups"], params.get["bias"])
+
+def conv3d(params):
+    return nn.Conv1d(params["in_channels"], params["out_channels"], params["kernel_size"], params.get["stride"], params.get["padding"], params.get["padding_mode"], params.get["dilation"], params.get["groups"], params.get["bias"])
+
+def tanh(params):
+    return nn.Tanh
+
 modules = {
-    "linear" : linear,
     "relu" : relu,
+    "tanh" : tanh,
+    "linear" : linear,
+    "conv1d" : conv1d,
+    "conv2d" : conv2d,
+    "conv3d" : conv3d,
 }
