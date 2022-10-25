0b9bf5c23291dd122b4b021a3cf3cbba4a29a5c9
cliebig2019 <82951475+cliebig2019@users.noreply.github.com>
2022-10-25 16:42:05 +0200

    Change json format to read

--- rosnav/model/custom_policy_utils/utils.py
+++ rosnav/model/custom_policy_utils/utils.py
@@ -2,18 +2,6 @@ import json
 from torch import nn
 from .modules import modules
 
-# Parses the parameters to a dictionary if module contains parameters
-def parse_parameters(module):
-    if not "params" in module:
-        return {}
-       
-    params = module["params"]
-    parsed_parameters = {}
-    for param in params:
-        parsed_parameters[param["param"]] = param["value"]
-    return parsed_parameters
-    
-
 def read_json(path):
     with open(path, 'r') as f:
         return json.load(f)
@@ -28,11 +16,8 @@ def create_body_network(data):
     # Iterate over each module given in the json in the policy
     for module in data["policy"]:
 
-        # Parse parameters for module
-        parsed_parameters = parse_parameters(module)
-
         # Add each module to the body network with corresponding number of module
-        body_net.add_module(f'{module_number}', modules[module["name"].lower()](parsed_parameters))
+        body_net.add_module(f'{module_number}', modules[module["type"].lower()](module))
 
         module_number += 1
 
