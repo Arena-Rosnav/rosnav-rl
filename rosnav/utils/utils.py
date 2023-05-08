eaaccecd3ac7989a7abb585f337c32467a888e86
tuananhroman <anh.maxiking@hotmail.de>
2023-05-08 18:55:51 +0200

    support vecnormalize loading

--- rosnav/utils/utils.py
+++ rosnav/utils/utils.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Tuple
 
@@ -5,6 +6,8 @@ import numpy as np
 import rospkg
 import rospy
 import yaml
+
+from stable_baselines3.common.vec_env import VecNormalize, DummyVecEnv
 from gym import spaces
 from rosnav.utils.constants import RosnavEncoder
 
@@ -26,21 +29,22 @@ def get_laser_from_robot_yaml(robot_model: str = None) -> Tuple[int, int, int, i
         laser_data = robot_data["laser"]
 
         rospy.set_param(
-            os.path.join(
-                rospy.get_namespace(), "laser/num_beams"
-            ), laser_data["num_beams"]
+            os.path.join(rospy.get_namespace(), "laser/num_beams"),
+            laser_data["num_beams"],
         )
 
         return (
-            laser_data["num_beams"], 
-            laser_data["angle"]["min"], 
+            laser_data["num_beams"],
+            laser_data["angle"]["min"],
             laser_data["angle"]["max"],
-            laser_data["angle"]["increment"]
+            laser_data["angle"]["increment"],
         )
 
 
 def get_observation_space_from_file(robot_model: str = None) -> Tuple[int, int]:
-    robot_state_size, action_state_size = 2, rospy.get_param(rospy.get_namespace() + "action_state_size", 3)
+    robot_state_size, action_state_size = 2, rospy.get_param(
+        rospy.get_namespace() + "action_state_size", 3
+    )
     num_beams, _, _, _ = get_laser_from_robot_yaml(robot_model)
 
     num_beams = RosnavEncoder[get_robot_space_encoder()]["lasers_to_adapted"](num_beams)
@@ -67,3 +71,31 @@ def stack_spaces(*ss) -> spaces.Box:
         high.extend(space.high.tolist())
 
     return spaces.Box(np.array(low).flatten(), np.array(high).flatten())
+
+
+def load_json(file_path: str) -> dict:
+    with open(file_path) as file:
+        return json.load(file)
+
+
+def load_yaml(file_path: str) -> dict:
+    with open(file_path) as file:
+        return yaml.load(file, Loader=yaml.FullLoader)
+
+
+def make_mock_env(config: dict) -> DummyVecEnv:
+    import rl_utils.envs.flatland_gym_env as flatland_gym_env
+
+    def _init():
+        return flatland_gym_env.FlatlandEnv(
+            ns="",
+            reward_fnc=config["reward_fnc"],
+            is_action_space_discrete=config["discrete_action_space"],
+            requires_task_manager=False,
+        )
+
+    return DummyVecEnv([_init])
+
+
+def load_vec_normalize(path: str, config: dict) -> VecNormalize:
+    return VecNormalize.load(path, make_mock_env(config))
