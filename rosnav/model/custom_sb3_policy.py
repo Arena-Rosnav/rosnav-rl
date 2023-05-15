86b9d3fba2281a00ca8041cfc76cafeae4850db9
tuananhroman <anh.maxiking@hotmail.de>
2023-05-15 19:42:22 +0200

    adjusted agent 30

--- rosnav/model/custom_sb3_policy.py
+++ rosnav/model/custom_sb3_policy.py
@@ -246,10 +246,10 @@ class AGENT_29(BaseAgent):
         self.robot_model = robot_model
 
 
-# framestacking
+# lstm
 @AgentFactory.register("AGENT_30")
 class AGENT_30(BaseAgent):
-    type = PolicyType.CNN
+    type = PolicyType.MLP_LSTM
     features_extractor_class = EXTRACTOR_7
     features_extractor_kwargs = dict(features_dim=512)
     net_arch = [dict(pi=[256, 256, 64], vf=[256, 256])]
