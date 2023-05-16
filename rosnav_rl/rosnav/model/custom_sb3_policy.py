50e6cfc788fd0660981fd523c38a584a210d8d73
sudo-Boris <boris.meinardus00@gmail.com>
2023-05-16 03:56:47 +0200

    agent 32 updated

--- rosnav_rl/rosnav/model/custom_sb3_policy.py
+++ rosnav_rl/rosnav/model/custom_sb3_policy.py
@@ -282,9 +282,9 @@ class AGENT_32(BaseAgent):
     type = PolicyType.MLP_LSTM
     features_extractor_class = EXTRACTOR_7
     features_extractor_kwargs = dict(features_dim=512)
-    net_arch = dict(pi=[512, 256, 128], vf=[256, 256, 128])
+    net_arch = dict(pi=[512, 256], vf=[256, 256])
     activation_fn = nn.ReLU
-    n_lstm_layers = 8
+    n_lstm_layers = 6
     lstm_hidden_size = 64
     shared_lstm = False
     enable_critic_lstm = True
