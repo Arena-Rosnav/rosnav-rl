74546eca779a57fe07338469320fd7e42401e283
tuananhroman <anh.maxiking@hotmail.de>
2023-05-17 13:09:44 +0200

    agent 44

--- rosnav_rl/rosnav/model/custom_sb3_policy.py
+++ rosnav_rl/rosnav/model/custom_sb3_policy.py
@@ -466,3 +466,20 @@ class AGENT_40(BaseAgent):
 
     def __init__(self, robot_model: str = None):
         self.robot_model = robot_model
+
+
+# lstm + framestacking
+@AgentFactory.register("AGENT_44")
+class AGENT_44(BaseAgent):
+    type = PolicyType.MLP_LSTM
+    features_extractor_class = EXTRACTOR_7
+    features_extractor_kwargs = dict(features_dim=256)
+    net_arch = [128, 64, 64]
+    activation_fn = nn.ReLU
+    n_lstm_layers = 2
+    lstm_hidden_size = 512
+    shared_lstm = True
+    enable_critic_lstm = False
+
+    def __init__(self, robot_model: str = None):
+        self.robot_model = robot_model
