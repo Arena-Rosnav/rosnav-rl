c2bd4cdcff4902ed860e5f62c4fd1aa88d1ff11d
tuananhroman <anh.maxiking@hotmail.de>
2023-05-20 16:51:35 +0200

    load normalize & stacked container for deployment

--- rosnav_rl/scripts/rosnav_node.py
+++ rosnav_rl/scripts/rosnav_node.py
@@ -1,23 +1,33 @@
-import rospy
-import rospkg
+import json
 import os
 import sys
 import traceback
-import json
-from stable_baselines3 import PPO
 
-from rosnav.srv import GetAction, GetActionResponse
-from rosnav.rosnav_space_manager.rosnav_space_manager import RosnavSpaceManager
-from rosnav.utils.constants import VALID_CONFIG_NAMES
-
+import numpy as np
+import rospkg
+import rospy
 from rosnav import *
-from rosnav.utils.utils import load_json, load_yaml, load_vec_normalize
+from rosnav.model.agent_factory import AgentFactory
+from rosnav.model.custom_sb3_policy import *
+from rosnav.model.base_agent import PolicyType
+from rosnav.msg import ResetStackedObs
+from rosnav.rosnav_space_manager.rosnav_space_manager import RosnavSpaceManager
+from rosnav.srv import GetAction, GetActionResponse
+from rosnav.utils.constants import VALID_CONFIG_NAMES
+from rosnav.utils.utils import load_json, load_vec_normalize, load_yaml, make_mock_env
+from sb3_contrib import RecurrentPPO
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env.stacked_observations import StackedObservations
 
 sys.modules["rl_agent"] = sys.modules["rosnav"]
 sys.modules["rl_utils.rl_utils.utils"] = sys.modules["rosnav.utils"]
 
 
 class RosnavNode:
+    DEFAULT_DONES = np.array([[False]])
+    DEFAULT_INFOS = [{}]
+    DEFAULT_EPS_START = np.array([True])
+
     def __init__(self):
         # Agent name and path
         self.agent_name = rospy.get_param("agent_name")
@@ -37,56 +47,116 @@ class RosnavNode:
             self._hyperparams["rl_agent"]
         )
 
+        # Load observation normalization and frame stacking
+        self._normalized_mode = self._hyperparams["rl_agent"]["normalize"]
+        self._stacked_mode = (
+            self._hyperparams["rl_agent"]["frame_stacking"]["enabled"]
+            if "frame_stacking" in self._hyperparams["rl_agent"]
+            else False
+        )
+
+        if self._normalized_mode:
+            self._vec_normalize = self._get_vec_normalize(self.agent_path)
+
+        if self._stacked_mode:
+            rospy.set_param("space_encoder", "StackedEncoder")
+            self._stacked_obs_container = self._get_stacked_obs(self._hyperparams)
+
         # Set RosnavSpaceEncoder as Middleware
         self._encoder = RosnavSpaceManager()
 
         # Load the model
-        self._agent = self._get_model(self.agent_path)
-
-        # Load VecNormalize
-        if self._hyperparams["rl_agent"]["normalize"]:
-            self._vec_normalize = self._get_vec_normalize(self.agent_path)
+        self._agent = self._get_model(self._hyperparams, self.agent_path)
 
         self._get_next_action_srv = rospy.Service(
             "rosnav/get_action", GetAction, self._handle_next_action_srv
         )
+        self._sub_reset_stacked_obs = rospy.Subscriber(
+            "rosnav/reset_stacked_obs", ResetStackedObs, self._reset_stacked_obs
+        )
 
-    def _handle_next_action_srv(self, request):
-        observation = self._encoder.encode_observation(
+        self.state = None
+        self._reset_state = True
+
+    def _encode_observation(self, obs_msg: GetAction):
+        return self._encoder.encode_observation(
             {
-                "goal_in_robot_frame": request.goal_in_robot_frame,
-                "laser_scan": request.laser_scan,
-                "last_action": request.last_action,
+                "goal_in_robot_frame": obs_msg.goal_in_robot_frame,
+                "laser_scan": obs_msg.laser_scan,
+                "last_action": obs_msg.last_action,
             },
             self._obs_structure,
         )
 
-        try:
-            if self._hyperparams["rl_agent"]["normalize"]:
-                observation = self._vec_normalize.normalize_obs(observation)
-        except ValueError as e:
-            rospy.logerr(e)
-            rospy.logerr(
-                "Check if the configuration file correctly specifies the observation space."
-            )
-            rospy.signal_shutdown("")
+    def _handle_next_action_srv(self, request: GetAction):
+        observation = self._encode_observation(request)
 
-        action = self._agent.predict(observation, deterministic=True)[0]
+        if self._normalized_mode:
+            try:
+                observation = self._vec_normalize.normalize_obs(observation)
+            except ValueError as e:
+                rospy.logerr(e)
+                rospy.logerr(
+                    "Check if the configuration file correctly specifies the observation space."
+                )
+                rospy.signal_shutdown("")
+
+        if self._stacked_mode:
+            observation, _ = self._stacked_obs_container.update(
+                observation, self.DEFAULT_DONES, self.DEFAULT_INFOS
+            )
+
+        predict_dict = {"observation": observation, "deterministic": True}
+
+        if self._recurrent_arch:
+            predict_dict.update(
+                {
+                    "state": self.state,
+                    "episode_start": self.DEFAULT_EPS_START
+                    if self._reset_state
+                    else None,
+                }
+            )
+
+        action, self.state = self._agent.predict(**predict_dict)
 
         decoded_action = self._encoder.decode_action(action)
 
         response = GetActionResponse()
         response.action = decoded_action
 
+        self._reset_state = False
+
         return response
 
-    def _get_model(self, agent_path):
+    def _reset_stacked_obs(self, request: GetAction):
+        self._reset_state = True
+
+        if self._stacked_mode:
+            observation = self._encode_observation(request)
+            self._stacked_obs_container.reset(observation)
+
+    def _get_model(self, hyperparams: dict, agent_path: str):
         action_state_sizes = [0, 3]
+
+        try:
+            arch_name = hyperparams["rl_agent"]["architecture_name"]
+        except KeyError:
+            net_type = None
+        finally:
+            net_type: PolicyType = AgentFactory.registry[arch_name].type
 
         for size in action_state_sizes:
             rospy.set_param(f"{rospy.get_namespace()}action_state_size", size)
             try:
-                return PPO.load(os.path.join(agent_path, "best_model.zip")).policy
+                if not net_type or net_type != PolicyType.MLP_LSTM:
+                    self._recurrent_arch = False
+                    return PPO.load(os.path.join(agent_path, "best_model.zip")).policy
+                else:
+                    self._recurrent_arch = True
+                    return RecurrentPPO.load(
+                        os.path.join(agent_path, "best_model.zip")
+                    ).policy
             except:
                 pass
 
@@ -100,9 +170,12 @@ class RosnavNode:
             cfg_path = os.path.join(agent_path, cfg_name)
             if os.path.isfile(cfg_path):
                 if cfg_name.endswith(".json"):
-                    return {"rl_agent": load_json(cfg_path)}
+                    cfg_dict = {"rl_agent": load_json(cfg_path)}
                 elif cfg_name.endswith(".yaml"):
-                    return load_yaml(cfg_path)
+                    cfg_dict = load_yaml(cfg_path)
+
+                assert cfg_dict is not None, "Config file is empty."
+                return cfg_dict
         raise ValueError("No valid config file found in agent folder.")
 
     def _get_observation_space_structure(self, hyperparams):
@@ -114,6 +187,15 @@ class RosnavNode:
     def _get_vec_normalize(self, agent_path):
         vec_normalize_path = os.path.join(agent_path, "vec_normalize.pkl")
         return load_vec_normalize(vec_normalize_path, self._hyperparams["rl_agent"])
+
+    def _get_stacked_obs(self, hyperparams: dict):
+        venv = make_mock_env(hyperparams["rl_agent"])
+        return StackedObservations(
+            1,
+            hyperparams["rl_agent"]["frame_stacking"]["stack_size"],
+            venv.observation_space,
+            "first",
+        )
 
 
 if __name__ == "__main__":
