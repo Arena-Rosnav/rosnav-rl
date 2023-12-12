6600e5c0b2090ef5933f40b0160eb619b0ad810a
tuananhroman <anh.maxiking@hotmail.de>
2023-12-13 00:59:35 +0100

    updated space manager

--- rosnav/rosnav_space_manager/rosnav_space_manager.py
+++ rosnav/rosnav_space_manager/rosnav_space_manager.py
@@ -19,29 +19,41 @@ import rospy
 
 class RosnavSpaceManager:
     def __init__(self):
+        self._stacked = rospy.get_param("rl_agent/frame_stacking/enabled")
         self._laser_num_beams = rospy.get_param("laser/num_beams")
         self._laser_max_range = rospy.get_param("laser/range")
         self._radius = rospy.get_param("robot_radius")
         self._is_holonomic = rospy.get_param("is_holonomic")
 
-        encoder_name = rospy.get_param("space_encoder", "DefaultEncoder")
-
-        is_action_space_discrete = rospy.get_param("is_action_space_discrete", False)
+        is_action_space_discrete = rospy.get_param(
+            "rl_agent/action_space/discrete", False
+        )
         actions = (
             rospy.get_param("actions/discrete")
             if is_action_space_discrete
             else rospy.get_param("actions/continuous")
         )
 
+        encoder_name = self._determine_encoder_name()
+
         self._encoder = BaseSpaceEncoderFactory.instantiate(
             encoder_name,
-            self._laser_num_beams,
-            self._laser_max_range,
-            self._radius,
-            self._is_holonomic,
-            actions,
-            is_action_space_discrete,
+            laser_num_beams=self._laser_num_beams,
+            laser_max_range=self._laser_max_range,
+            radius=self._radius,
+            is_holonomic=self._is_holonomic,
+            actions=actions,
+            is_action_space_discrete=is_action_space_discrete,
+            stacked=self._stacked,
         )
+
+    def _determine_encoder_name(self) -> str:
+        if rospy.get_param("rl_agent/reduce_num_beams/enabled", False):
+            return "ReducedLaserEncoder"
+        if rospy.get_param("rl_agent/resnet", False):
+            return "SemanticResNetSpaceEncoder"
+        else:
+            return "DefaultEncoder"
 
     def get_observation_space(self):
         return self._encoder.get_observation_space()
