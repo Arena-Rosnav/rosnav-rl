420959eef64b64b8083878c8b2c1b5cf64e3d280
tuananhroman <anh.maxiking@hotmail.de>
2023-12-16 19:41:29 +0100

    adjusted base classes

--- rosnav_rl/rosnav/utils/observation_space/spaces/base_observation_space.py
+++ rosnav_rl/rosnav/utils/observation_space/spaces/base_observation_space.py
@@ -6,6 +6,21 @@ import numpy as np
 
 
 class BaseObservationSpace(ABC):
+    def __init__(self, *args, **kwargs) -> None:
+        self._space = self.get_gym_space()
+
+    @property
+    def space(self) -> spaces.Space:
+        return self._space
+
+    @property
+    def shape(self):
+        return self._space.shape
+
     @abstractmethod
     def get_gym_space(self) -> spaces.Space:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def encode_observation(self, observation: dict, *args, **kwargs) -> np.ndarray:
         raise NotImplementedError()
