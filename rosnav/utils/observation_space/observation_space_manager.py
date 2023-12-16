e7dfec66cd420a4795cf25977b2c76bd057c5b46
tuananhroman <anh.maxiking@hotmail.de>
2023-12-16 19:41:29 +0100

    adjusted base classes

--- rosnav/utils/observation_space/observation_space_manager.py
+++ rosnav/utils/observation_space/observation_space_manager.py
@@ -1,30 +1,25 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
+import numpy as np
 from gymnasium import spaces
 
-from .observation_space_factory import SpaceFactory
-from .space_index import SPACE_FACTORY_KEYS
-from .spaces.base.goal_space import GoalSpace
-from .spaces.base.laser_space import LaserScanSpace
-from .spaces.base.last_action_space import LastActionSpace
-from .spaces.feature_maps.stacked_laser_map_space import StackedLaserMapSpace
-from .spaces.feature_maps.pedestrian_location_space import PedestrianLocationSpace
-from .spaces.feature_maps.pedestrian_type_space import PedestrianTypeSpace
-from .spaces.feature_maps.pedestrian_vel_x_space import PedestrianVelXSpace
-from .spaces.feature_maps.pedestrian_vel_y_space import PedestrianVelYSpace
+from .space_index import SPACE_INDEX
+from .spaces.base_observation_space import BaseObservationSpace
 from .utils import stack_spaces
 
 
 class ObservationSpaceManager:
     def __init__(
         self,
-        space_list: List[str],
+        space_list: List[Union[str, SPACE_INDEX]],
         space_kwargs: Dict[str, Any],
-        enable_frame_stacking: bool = False,
+        frame_stacking: bool = False,
+        flatten: bool = True,
     ) -> None:
         self._spacelist = space_list
         self._space_kwargs = space_kwargs
-        self._enable_frame_stacking = enable_frame_stacking
+        self._frame_stacking = frame_stacking
+        self._flatten = flatten
 
         self._setup_spaces()
 
@@ -33,50 +28,63 @@ class ObservationSpaceManager:
         return self._spacelist
 
     @property
-    def unified_observation_space(self) -> spaces.Box:
+    def observation_space(self) -> spaces.Box:
         return stack_spaces(
-            *self._observation_spaces.values(),
-            frame_stacking_enabled=self._enable_frame_stacking,
+            *(
+                self._space_containers[name].space
+                for name in self._space_containers.keys()
+            ),
+            frame_stacking_enabled=self._frame_stacking,
         )
 
-    def _setup_spaces(self):
-        self._space_containers = self._generate_space_container(
-            self._spacelist, self._space_kwargs
-        )
-        self._observation_spaces = self._generate_observation_spaces(
-            self._space_containers
-        )
-
-    def _generate_space_container(
-        self,
-        names: List[str],
-        space_kwargs: Dict[str, Any],
-    ) -> Dict[str, "BaseObservationSpace"]:
-        return {
-            name: SpaceFactory.instantiate(
-                SPACE_FACTORY_KEYS[name].value, **space_kwargs
-            )
-            for name in names
-        }
-
-    def _generate_observation_spaces(
-        self, space_containers: Dict[str, "BaseObservationSpace"]
-    ) -> Dict[str, "BaseObservationSpace"]:
-        return {
-            name: container.get_gym_space()
-            for name, container in space_containers.items()
-        }
-
-    def add_observation_space(self, space: str):
+    def add_observation_space(self, space: SPACE_INDEX):
+        assert isinstance(space, SPACE_INDEX), "Invalid Space Type"
         assert (
             space not in self._spacelist
         ), f"{space}-ObservationSpace was already specified!"
         self._spacelist.append(space)
         self._setup_spaces()
 
-    def add_multiple_observation_spaces(self, space_list: List[str]):
+    def add_multiple_observation_spaces(self, space_list: List[SPACE_INDEX]):
         for space in space_list:
             self.add_observation_space(space)
 
-    def get_single_observation_space(self, name: str) -> spaces.Box:
-        return self._observation_spaces[name]
+    def encode_observation(self, observation: dict, *args, **kwargs) -> np.ndarray:
+        return np.concatenate(
+            [
+                self._space_containers[space.name].encode_observation(
+                    observation, **kwargs
+                )
+                for space in self._spacelist
+            ],
+            axis=0 if self._frame_stacking else 1,
+        )
+
+    @staticmethod
+    def get_space_index(space_name: Union[str, SPACE_INDEX]) -> SPACE_INDEX:
+        if isinstance(space_name, SPACE_INDEX):
+            return space_name
+        return SPACE_INDEX[space_name.upper()]
+
+    def _setup_spaces(self):
+        self._space_containers = self._generate_space_container(
+            self._spacelist, self._space_kwargs
+        )
+
+    def _generate_space_container(
+        self,
+        space_list: List[Union[str, SPACE_INDEX]],
+        space_kwargs: Dict[str, Any],
+    ) -> Dict[str, BaseObservationSpace]:
+        space_list = [
+            ObservationSpaceManager.get_space_index(space) for space in space_list
+        ]
+        return {
+            space_index.name: space_index.value(flatten=self._flatten, **space_kwargs)
+            for space_index in space_list
+        }
+
+    def __getitem__(self, space_name: Union[str, SPACE_INDEX]) -> spaces.Box:
+        if isinstance(space_name, SPACE_INDEX):
+            space_name = space_name.name
+        return self._space_containers[space_name.upper()].space
---
+++ rosnav/utils/observation_space/spaces/feature_maps/base_feature_map_space.py
@@ -0,0 +1,58 @@
+from typing import List
+
+import numpy as np
+from pedsim_msgs.msg import SemanticDatum
+
+from ..base_observation_space import BaseObservationSpace
+
+
+class BaseFeatureMapSpace(BaseObservationSpace):
+    def __init__(
+        self,
+        feature_map_size: int,
+        roi_in_m: float,
+        flatten: bool = True,
+        *args,
+        **kwargs
+    ) -> None:
+        self._feature_map_size = feature_map_size
+        self._roi_in_m = roi_in_m
+        self._flatten = flatten
+
+        self._space = self.get_gym_space()
+
+    def _get_map_index(self, position: tuple) -> tuple:
+        x, y, *_ = position
+        x = int((x / self._roi_in_m) * self._feature_map_size) + (
+            self._feature_map_size // 2 - 1
+        )
+        y = int((y / self._roi_in_m) * self._feature_map_size) + (
+            self._feature_map_size // 2 - 1
+        )
+        x = min(max(x, 0), self._feature_map_size - 1)
+        y = min(max(y, 0), self._feature_map_size - 1)
+        return x, y
+
+    def _get_semantic_map(
+        self, semantic_data: List[SemanticDatum], robot_pose
+    ) -> np.ndarray:
+        pos_map = np.zeros((self._feature_map_size, self._feature_map_size))
+        map_size = pos_map.shape[0]
+
+        for data in semantic_data:
+            relative_pos = BaseFeatureMapSpace.get_relative_pos(
+                data.location, robot_pose
+            )
+            index = self._get_map_index(relative_pos)
+            if 0 <= index[0] < map_size and 0 <= index[1] < map_size:
+                pos_map[index] = data.evidence
+
+        return pos_map
+
+    @staticmethod
+    def get_relative_pos(reference_frame, distant_frame) -> tuple:
+        return (
+            distant_frame.x - reference_frame.x,
+            distant_frame.y - reference_frame.y,
+            distant_frame.z - reference_frame.z,
+        )
