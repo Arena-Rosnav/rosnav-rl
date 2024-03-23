ac1cc16ff08f09e0d95ece993a902de8d6a2c6b0
Max <schreffmax@gmail.com>
2024-03-23 19:56:30 +0900

    Forward pass for Resnet

--- rosnav/model/feature_extractors/resnet/utils.py
+++ rosnav/model/feature_extractors/resnet/utils.py
@@ -1,48 +1,7 @@
-from torch import nn
+from torch import nn, Tensor
 from typing import List, Union, Optional, Callable
 import numpy as np
-from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1, conv2x2
-
-
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1) -> nn.Conv2d:
-    """
-    3x3 convolution with padding
-
-    Args:
-        in_planes (int): Number of input channels
-        out_planes (int): Number of output channels
-        stride (int, optional): Stride for the convolution. Defaults to 1.
-        groups (int, optional): Number of groups for grouped convolution. Defaults to 1.
-        dilation (int, optional): Dilation rate for dilated convolution. Defaults to 1.
-
-    Returns:
-        nn.Conv2d: 3x3 convolutional layer with specified parameters
-    """
-    return nn.Conv2d(
-        in_planes,
-        out_planes,
-        kernel_size=3,
-        stride=stride,
-        padding=dilation,
-        groups=groups,
-        bias=False,
-        dilation=dilation,
-    )
-
-
-def conv1x1(in_planes, out_planes, stride=1) -> nn.Conv2d:
-    """
-    1x1 convolution
-
-    Args:
-        in_planes (int): Number of input channels
-        out_planes (int): Number of output channels
-        stride (int, optional): Stride for the convolution. Defaults to 1.
-
-    Returns:
-        nn.Conv2d: 1x1 convolutional layer with specified parameters
-    """
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1
 
 
 class ResNet(nn.Module):
@@ -81,6 +40,7 @@ class ResNet(nn.Module):
         self.res_layer4 = self._make_layer(
             block_count=layer_sizes[3], planes=self.base_planes * 2 * 2 * 2, stride=2
         )
+        
         
         
     def _make_layer(
@@ -125,5 +85,17 @@ class ResNet(nn.Module):
             )
             
         return nn.Sequential(*layers)
+    
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.res_layer1(x)
+        x = self.res_layer2(x)
+        x = self.res_layer3(x)
+        x = self.res_layer4(x)
+
         
         
