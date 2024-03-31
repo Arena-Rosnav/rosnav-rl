3494c7cb424070cddbcdaf63f64444429a88a4ca
Max <schreffmax@gmail.com>
2024-03-31 17:18:14 +0900

    Complete RGBD feature extractor

--- rosnav/model/feature_extractors/rgbd/rgbd_feature_nets.py
+++ rosnav/model/feature_extractors/rgbd/rgbd_feature_nets.py
@@ -7,6 +7,7 @@ from torch.nn.modules import BatchNorm2d, Module
 import rospy
 import torch
 import torch.nn as nn
+from torch import Tensor
 from rosnav.utils.observation_space.observation_space_manager import (
     ObservationSpaceManager,
 )
@@ -27,15 +28,23 @@ class RESNET_RGBD_FUSION_EXTRACTOR_1(RosnavBaseExtractor):
     def __init__(self,
                  observation_space: gym.spaces.Box,
                  observation_space_manager: ObservationSpaceManager,
+                 image_height: int,
+                 image_width: int,
                  features_dim: int = 512 + 32 + 32,
                  rgbd_backbone: Callable[..., ResNet] = resnet50_groupnorm,
                  rgbd_out_dim: int = 512,
+                 goal_out_dim: int = 32,
+                 last_action_out_dim: int = 32,
                  *args,
                  **kwargs
                  ):
         self._features_dim = features_dim
         self._rgbd_backbone = rgbd_backbone
         self._rgbd_out_dim = rgbd_out_dim
+        self._image_height = image_height
+        self._image_width = image_width
+        self._goal_out_dim = goal_out_dim
+        self._last_action_out_dim = last_action_out_dim
         
         self._observation_space_manager = observation_space_manager
         
@@ -50,14 +59,62 @@ class RESNET_RGBD_FUSION_EXTRACTOR_1(RosnavBaseExtractor):
     def _get_input_sizes(self):
         self._goal_size = self._observation_space_manager[SPACE_INDEX.GOAL].shape[-1]
         self._last_action_size = self._observation_space_manager[SPACE_INDEX.LAST_ACTION].shape[-1]
-        self.
+        self._image_size = 4 * self._image_height * self._image_width
     
     def _setup_network(self, *args, **kwargs):
         # RGBD part
-        self.perception_net = RgbdPerceptionNet(self._rgbd_out_dim, self._rgbd_backbone, kwargs)
+        self.visual_net = RgbdPerceptionNet(self._rgbd_out_dim, self._rgbd_backbone, kwargs)
         
         # goal part
-        self.goal_fc = nn.Linear(in_features=)
+        self.goal_net = nn.Sequential(
+            nn.Linear(
+                in_features=self._goal_size,
+                out_features=self._goal_out_dim
+            ),
+            nn.ReLU(inplace=True)
+        )
+        
+        # last action part
+        self.last_action_net = nn.Sequential(
+            nn.Linear(
+                in_features=self._last_action_size, 
+                out_features=self._last_action_out_dim
+            ),
+            nn.ReLU(inplace=True)
+        )
+        
+        # fusion
+        self.fc = nn.Linear(
+            in_features=self._rgbd_out_dim + self._goal_out_dim + self._last_action_out_dim,
+            out_features=self._features_dim
+        )
     
-    def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        pass
+    def _forward_impl(self, image: Tensor, goal: Tensor, last_action: Tensor) -> Tensor:
+        # seperate nets
+        visual_out = self.visual_net(image)
+        goal_out = self.goal_net(goal)
+        last_action_out = self.last_action_net(last_action)
+        
+        # concatenation
+        cat_out = torch.cat([visual_out, goal_out, last_action_out], dim=1)
+        
+        # fusion fully connected
+        out = self.fc(cat_out)
+        
+        return out
+    
+    def _forward(self, observations: Tensor) -> Tensor:
+        # extract seperate inputs
+        image_vector = observations[:, :self._image_size]
+        image = image_vector.reshape(-1, 4, self._image_height, self._image_width)
+        
+        goal_vector = observations[:, self._image_size:self._image_size + self._goal_size]
+        goal = goal_vector.reshape(-1, self._goal_size)
+        
+        last_action_vector = observations[:, -self._last_action_size:]
+        last_action = last_action_vector.reshape(-1, self._last_action_size)
+        
+        return self._forward_impl(image, goal, last_action)
+    
+    def forward(self, observations: Tensor) -> Tensor:
+        return self._forward(observations)
