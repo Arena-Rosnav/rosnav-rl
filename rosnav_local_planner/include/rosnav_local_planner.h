2524b12d01595c68452c8e5c61682da8021bb6b7
tuananhroman <anh.maxiking@hotmail.de>
2024-05-14 20:05:09 +0200

    mbf interface

---
+++ rosnav_local_planner/include/rosnav_local_planner.h
@@ -0,0 +1,171 @@
+#ifndef ROSNAV_LOCAL_PLANNER_H
+#define ROSNAV_LOCAL_PLANNER_H
+
+#include <ros/ros.h>
+
+// base local planner base class and utilities
+#include <nav_core/base_local_planner.h>
+#include <mbf_costmap_core/costmap_controller.h>
+#include <base_local_planner/goal_functions.h>
+#include <base_local_planner/odometry_helper_ros.h>
+
+// transforms
+#include <tf/transform_listener.h>
+
+// costmap
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_converter/costmap_converter_interface.h>
+
+// dynamic reconfigure
+#include <rosnav_local_planner/RosnavLocalPlannerReconfigureConfig.h>
+#include <dynamic_reconfigure/server.h>
+// #include <rosnav/srv/GetAction.h>
+
+#include <mbf_msgs/ExePathResult.h>
+
+#include <rosnav/GetAction.h>
+
+
+namespace rosnav_local_planner
+{
+    class RosnavLocalPlanner : public nav_core::BaseLocalPlanner,  public mbf_costmap_core::CostmapController
+    {
+    public:
+        /**
+        * @brief Default constructor of the rosnav plugin
+        */
+        RosnavLocalPlanner();
+
+        /**
+        * @brief Destructor of rosnav plugin
+        */
+        ~RosnavLocalPlanner();
+
+        /**
+         * @brief  Constructs the local planner
+         * @param name The name to give this instance of the local planner
+         * @param tf A pointer to a transform listener
+         * @param costmap_ros The cost map to use for assigning costs to local plans
+         */
+        void initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros);
+
+
+        /**
+        * @brief Initializes the rosn plugin
+        * @param name The name of the instance
+        * @param tf Pointer to a transform listener
+        * @param costmap_ros Cost map representing occupied and free space
+        */
+        void initialize();
+
+        /**
+        * @brief Set the plan that the rosnav local planner is following
+        * @param orig_global_plan The plan to pass to the local planner
+        * @return True if the plan was updated successfully, false otherwise
+        */
+        bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
+
+        /**
+        * @brief Given the current position, orientation, and velocity of the robot, compute velocity commands to send to the base
+        * @param cmd_vel Will be filled with the velocity command to be passed to the robot base
+        * @return True if a valid trajectory was found, false otherwise
+        */
+        bool computeVelocityCommands(geometry_msgs::Twist& cmd_vel);
+
+        /**
+        * @brief Given the current position, orientation, and velocity of the robot, compute velocity commands to send to the base.
+        * @remark Extended version for MBF API
+        * @param pose the current pose of the robot.
+        * @param velocity the current velocity of the robot.
+        * @param cmd_vel Will be filled with the velocity command to be passed to the robot base.
+        * @param message Optional more detailed outcome as a string
+        * @return Result code as described on ExePath action result:
+        *         SUCCESS         = 0
+        *         1..9 are reserved as plugin specific non-error results
+        *         FAILURE         = 100   Unspecified failure, only used for old, non-mfb_core based plugins
+        *         CANCELED        = 101
+        *         NO_VALID_CMD    = 102
+        *         PAT_EXCEEDED    = 103
+        *         COLLISION       = 104
+        *         OSCILLATION     = 105
+        *         ROBOT_STUCK     = 106
+        *         MISSED_GOAL     = 107
+        *         MISSED_PATH     = 108
+        *         BLOCKED_PATH    = 109
+        *         INVALID_PATH    = 110
+        *         TF_ERROR        = 111
+        *         NOT_INITIALIZED = 112
+        *         INVALID_PLUGIN  = 113
+        *         INTERNAL_ERROR  = 114
+        *         121..149 are reserved as plugin specific errors
+        */
+        uint32_t computeVelocityCommands(const geometry_msgs::PoseStamped& pose, const geometry_msgs::TwistStamped& velocity,
+                                        geometry_msgs::TwistStamped &cmd_vel, std::string &message);
+
+        bool retrieveVelocityCommands(geometry_msgs::TwistStamped &cmd_vel);
+
+        /**
+        * @brief  Check if the goal pose has been achieved
+        *
+        * The actual check is performed in computeVelocityCommands().
+        * Only the status flag is checked here.
+        * @return True if achieved, false otherwise
+        */
+        bool isGoalReached();
+
+        /**
+            * @brief Dummy version to satisfy MBF API
+            */
+        bool isGoalReached(double xy_tolerance, double yaw_tolerance) { return isGoalReached(); };
+
+        /**
+        * @brief Requests the planner to cancel, e.g. if it takes too much time
+        * @remark New on MBF API
+        * @return True if a cancel has been successfully requested, false if not implemented.
+        */
+        bool cancel() { return false; };
+
+        /**
+        * @brief Callback for the dynamic_reconfigure node.
+        * 
+        * This callback allows to modify parameters dynamically at runtime without restarting the node
+        * @param config Reference to the dynamic reconfigure config
+        * @param level Dynamic reconfigure level
+        */
+        void reconfigureCB(RosnavLocalPlannerReconfigureConfig& config, uint32_t level);
+
+        bool updateAgentSelection(std::string agent_name);
+
+        bool getServiceClient(std::string service_name, ros::ServiceClient& client, double_t timeout);
+
+        std::vector<std::string> translateStrToList(std::string str, std::string seperator);
+
+        /**
+         * @brief Return the internal config mutex
+        */
+        boost::mutex& configMutex() {return config_mutex_;}
+
+    private:
+        // local planner
+        // boost::shared_ptr<rosnavLocalPlannerROS> rosnav_local_planner_;
+
+        // dynamic reconfigure
+        // dynamic_reconfigure::Server<rosnav_local_planner::RosnavLocalPlanner> *dsrv_;
+
+        bool initialized_; //!< Keeps track about the correct initialization of this class
+        
+        ros::NodeHandle nh_; // ROS node handle
+        ros::ServiceClient client_; // service client for cmd vel retrieval
+
+        std::string active_agent;   // active agent name considered for cmd vel retrieval
+        std::vector<std::string> agent_selection_list;  
+        std::map<std::string, ros::ServiceClient> agent_srv_clients;
+
+        boost::shared_ptr< dynamic_reconfigure::Server<RosnavLocalPlannerReconfigureConfig> > dynamic_recfg_; //!< Dynamic reconfigure server to allow config modifications at runtime
+        boost::mutex config_mutex_; //!< Mutex for config accesses and changes
+
+    };
+}; // namespace rosnav_local_planner
+
+#endif // ROSNAV_LOCAL_PLANNER_H
+
---
+++ rosnav_local_planner/src/RosnavLocalPlanner.cpp
@@ -0,0 +1,198 @@
+#include <rosnav_local_planner.h>
+// pluginlib macros
+#include <pluginlib/class_list_macros.h>
+#include <ros/ros.h>
+
+
+// register this planner as a BaseLocalPlanner plugin
+PLUGINLIB_EXPORT_CLASS(rosnav_local_planner::RosnavLocalPlanner, nav_core::BaseLocalPlanner)
+PLUGINLIB_EXPORT_CLASS(rosnav_local_planner::RosnavLocalPlanner, mbf_costmap_core::CostmapController)
+
+rosnav_local_planner::RosnavLocalPlanner::RosnavLocalPlanner(): initialized_(false)
+{
+}
+
+rosnav_local_planner::RosnavLocalPlanner::~RosnavLocalPlanner()
+{
+}
+
+
+void rosnav_local_planner::RosnavLocalPlanner::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros)
+{
+    if (!initialized_)
+    {
+        // create Node handle
+        ros::NodeHandle nh_("~/" + name);
+
+        ROS_INFO("Initializing RosnavLocalPlanner...");
+
+        nh_.param("/agent_name", active_agent, std::string(""));
+
+        if (!active_agent.empty())
+        {
+            updateAgentSelection(active_agent);
+            client_ = agent_srv_clients[active_agent];
+        }
+        else
+        {
+            ROS_ERROR("No agent selected. Please provide an agent name in the parameter server or via dynamic reconfigure.");
+        }
+
+        // initialize dynamic reconfigure
+        dynamic_recfg_ = boost::make_shared< dynamic_reconfigure::Server<RosnavLocalPlannerReconfigureConfig> >(nh_);
+        dynamic_reconfigure::Server<RosnavLocalPlannerReconfigureConfig>::CallbackType cb = boost::bind(&RosnavLocalPlanner::reconfigureCB, this, _1, _2);
+        dynamic_recfg_->setCallback(cb);
+
+        initialized_ = true;
+    }
+}
+
+bool rosnav_local_planner::RosnavLocalPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan)
+{
+    // check if plugin is initialized
+    if (!initialized_)
+    {
+        ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+        return false;
+    }
+    ROS_DEBUG("Setting plan...");
+    return true;
+}
+
+bool rosnav_local_planner::RosnavLocalPlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
+{
+    // check if plugin is initialized
+    if (!initialized_)
+    {
+        ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+        return false;
+    }
+    ROS_ERROR("Method not implemented yet!");
+    // call service to retrieve cmd vel
+    return false;
+}
+
+u_int32_t rosnav_local_planner::RosnavLocalPlanner::computeVelocityCommands(const geometry_msgs::PoseStamped& pose, const geometry_msgs::TwistStamped& velocity,
+                                        geometry_msgs::TwistStamped &cmd_vel, std::string &message)
+{
+    // check if plugin is initialized
+    if (!initialized_)
+    {
+        ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+        return mbf_msgs::ExePathResult::INTERNAL_ERROR;
+    }
+    bool succ = retrieveVelocityCommands(cmd_vel);
+    return mbf_msgs::ExePathResult::SUCCESS;
+}
+
+bool rosnav_local_planner::RosnavLocalPlanner::retrieveVelocityCommands(geometry_msgs::TwistStamped &cmd_vel)
+{
+    ROS_DEBUG("Retrieving velocity commands...");
+
+    rosnav::GetAction srv;
+    if (client_.call(srv))
+    {
+        cmd_vel.twist.linear.x = srv.response.action[0];
+        cmd_vel.twist.linear.y = srv.response.action[1];
+        cmd_vel.twist.angular.z = srv.response.action[2];
+    }
+    else
+    {
+        ROS_ERROR("Failed to call service '%s'", client_.getService().c_str());
+        return false;
+    }
+    return true;
+}
+
+
+bool rosnav_local_planner::RosnavLocalPlanner::isGoalReached()
+{
+    // check if plugin is initialized
+    if (!initialized_)
+    {
+        ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+        return false;
+    }
+    ROS_DEBUG("isGoalReached(): Not implemented yet! (not needed for now)");
+    return false;
+}
+
+void rosnav_local_planner::RosnavLocalPlanner::reconfigureCB(RosnavLocalPlannerReconfigureConfig& config, uint32_t level)
+{
+    // check if plugin is initialized
+    if (!initialized_)
+    {
+        ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
+        return;
+    }
+
+    // check if the agent has changed
+    if (config.active_agent == active_agent)
+    {
+        return;
+    }
+
+    boost::mutex::scoped_lock l(config_mutex_);
+
+    // deal with new agent
+    if (!(std::find(agent_selection_list.begin(), agent_selection_list.end(), config.active_agent) != agent_selection_list.end())) {
+        ROS_INFO("New agent detected: %s", config.active_agent.c_str());
+        if (!(updateAgentSelection(config.active_agent)))
+        {
+            ROS_INFO("Keeping the current agent selection and most recently active agent '%s'", active_agent.c_str());
+        }
+    }
+
+    // update the active agent
+    active_agent = config.active_agent;
+    client_ = agent_srv_clients[active_agent];
+}
+
+std::vector<std::string> rosnav_local_planner::RosnavLocalPlanner::translateStrToList(std::string str, std::string separator)
+{
+    std::vector<std::string> result;
+    size_t pos = 0;
+    while ((pos = str.find(separator)) != std::string::npos)
+    {
+        result.push_back(str.substr(0, pos));
+        str.erase(0, pos + separator.length());
+    }
+    result.push_back(str);
+    return result;
+}
+
+
+bool rosnav_local_planner::RosnavLocalPlanner::updateAgentSelection(std::string agent_name)
+{
+    std::string service_name = "rosnav/" + agent_name + "/get_action";
+
+    ros::ServiceClient client;
+    if (getServiceClient(service_name, client, 10.0))
+    {
+        ROS_INFO("Service '%s' is online. Updating the agent selection list.", service_name.c_str());
+        agent_srv_clients.insert({agent_name, client});
+        agent_selection_list.push_back(agent_name);
+
+        ROS_INFO("Agent selection list:");
+        for (const auto& agent : agent_selection_list) {
+            ROS_INFO("- %s", agent.c_str());
+        }
+        return true;
+    }
+
+    ROS_ERROR("Service '%s' is not online. Check if the agent node is running. Is the service advertised in the agents' namespace?", service_name.c_str());
+    return false;
+}
+
+bool rosnav_local_planner::RosnavLocalPlanner::getServiceClient(std::string service_name, ros::ServiceClient& client, double_t timeout=10)
+{
+    // Wait for the service to become available
+    if (ros::service::waitForService(service_name, ros::Duration(timeout)))
+    {
+        // initialize service clients to retrieve cmd vel on "rosnav/*agent_name*/get_action"
+        client = nh_.serviceClient<rosnav::GetAction>(service_name);
+        return true;
+    }
+    return false;
+
+}
