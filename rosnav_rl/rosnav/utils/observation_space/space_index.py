e26118ab5a7b38a2376b9c987e23cefb526b12fb
tuananhroman <anh.maxiking@hotmail.de>
2024-06-12 18:23:20 +0200

    space_index for loading old models

--- rosnav_rl/rosnav/utils/observation_space/space_index.py
+++ rosnav_rl/rosnav/utils/observation_space/space_index.py
@@ -1,49 +1,52 @@
-# __all__ = [
-#     "LaserScanSpace",
-#     "GoalSpace",
-#     "LastActionSpace",
-#     "RGBDSpace",
-#     "PedestrianLocationSpace",
-#     "PedestrianTypeSpace",
-#     "PedestrianVelXSpace",
-#     "PedestrianVelYSpace",
-#     "StackedLaserMapSpace",
-#     "PedestrianSocialStateSpace",
-# ]
+__all__ = [
+    "LaserScanSpace",
+    "DistAngleToSubgoalSpace",
+    "LastActionSpace",
+    "RGBDSpace",
+    "PedestrianLocationSpace",
+    "PedestrianTypeSpace",
+    "PedestrianVelXSpace",
+    "PedestrianVelYSpace",
+    "StackedLaserMapSpace",
+    "PedestrianSocialStateSpace",
+]
+from enum import Enum
 
-# from ..observation_space.spaces.base.goal_space import GoalSpace
-# from ..observation_space.spaces.base.laser_space import LaserScanSpace
-# from ..observation_space.spaces.base.last_action_space import LastActionSpace
-# from ..observation_space.spaces.base.rgbd_space import RGBDSpace
-# from ..observation_space.spaces.feature_maps.pedestrian_location_space import (
-#     PedestrianLocationSpace,
-# )
-# from ..observation_space.spaces.feature_maps.pedestrian_type_space import (
-#     PedestrianTypeSpace,
-# )
-# from ..observation_space.spaces.feature_maps.pedestrian_vel_x_space import (
-#     PedestrianVelXSpace,
-# )
-# from ..observation_space.spaces.feature_maps.pedestrian_vel_y_space import (
-#     PedestrianVelYSpace,
-# )
-# from ..observation_space.spaces.feature_maps.stacked_laser_map_space import (
-#     StackedLaserMapSpace,
-# )
-# from .spaces.feature_maps.pedestrian_social_state_space import (
-#     PedestrianSocialStateSpace,
-# )
+from ..observation_space.spaces.base.dist_angle_to_subgoal_space import (
+    DistAngleToSubgoalSpace,
+)
+from ..observation_space.spaces.base.laser_space import LaserScanSpace
+from ..observation_space.spaces.base.last_action_space import LastActionSpace
+from ..observation_space.spaces.base.rgbd_space import RGBDSpace
+from ..observation_space.spaces.feature_maps.pedestrian_location_space import (
+    PedestrianLocationSpace,
+)
+from ..observation_space.spaces.feature_maps.pedestrian_type_space import (
+    PedestrianTypeSpace,
+)
+from ..observation_space.spaces.feature_maps.pedestrian_vel_x_space import (
+    PedestrianVelXSpace,
+)
+from ..observation_space.spaces.feature_maps.pedestrian_vel_y_space import (
+    PedestrianVelYSpace,
+)
+from ..observation_space.spaces.feature_maps.stacked_laser_map_space import (
+    StackedLaserMapSpace,
+)
+from .spaces.feature_maps.pedestrian_social_state_space import (
+    PedestrianSocialStateSpace,
+)
 
 
-# # class SPACE_INDEX(Enum):
-# #     LASER = LaserScanSpace
-# #     GOAL = GoalSpace
-# #     LAST_ACTION = LastActionSpace
-# #     RGBD = RGBDSpace
+class SPACE_INDEX(Enum):
+    LASER = LaserScanSpace
+    GOAL = DistAngleToSubgoalSpace
+    LAST_ACTION = LastActionSpace
+    RGBD = RGBDSpace
 
-# #     STACKED_LASER_MAP = StackedLaserMapSpace
-# #     PEDESTRIAN_LOCATION = PedestrianLocationSpace
-# #     PEDESTRIAN_TYPE = PedestrianTypeSpace
-# #     PEDESTRIAN_VEL_X = PedestrianVelXSpace
-# #     PEDESTRIAN_VEL_Y = PedestrianVelYSpace
-# #     PEDESTRIAN_SOCIAL_STATE = PedestrianSocialStateSpace
+    STACKED_LASER_MAP = StackedLaserMapSpace
+    PEDESTRIAN_LOCATION = PedestrianLocationSpace
+    PEDESTRIAN_TYPE = PedestrianTypeSpace
+    PEDESTRIAN_VEL_X = PedestrianVelXSpace
+    PEDESTRIAN_VEL_Y = PedestrianVelYSpace
+    PEDESTRIAN_SOCIAL_STATE = PedestrianSocialStateSpace
