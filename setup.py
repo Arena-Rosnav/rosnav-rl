794ddaab5d602b0595f95f85b960e0b644f5abff
tuananhroman <anh.maxiking@hotmail.de>
2024-10-15 20:33:41 +0200

    rename to rosnav_rl

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['rosnav']
-)
+setup_args = generate_distutils_setup(packages=["rosnav_rl"])
 
 setup(**setup_args)
