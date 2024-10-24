0f6288c43bf27bdd8891d14585cadca3047fbd06
tuananhroman <anh.maxiking@hotmail.de>
2024-10-24 13:06:08 +0200

    base rl_model refactor

--- rosnav_rl/rosnav_rl/model/model.py
+++ rosnav_rl/rosnav_rl/model/model.py
@@ -1,37 +1,48 @@
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any, Dict, List
 
-from rosnav_rl.spaces import EncodedObservationDict, BaseObservationSpace
-from typing import List, Dict, Any
+if TYPE_CHECKING:
+    from rosnav_rl.spaces import BaseObservationSpace, EncodedObservationDict
 
 
 class RL_Model(ABC):
     model = None
 
     @abstractmethod
-    @property
-    def observation_space_list(self) -> List[BaseObservationSpace]:
-        raise NotImplementedError()
-
-    @abstractmethod
-    @property
-    def observation_space_kwargs(self) -> Dict[str, Any]:
-        raise NotImplementedError()
-
-    @abstractmethod
-    @property
-    def config(self) -> Dict[str, Any]:
-        return {}
-
-    @abstractmethod
     def initialize(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def save(self, *args, **kwargs):
         pass
 
     @abstractmethod
     def load(self, *args, **kwargs):
         pass
 
-    def get_action(self, observation: EncodedObservationDict, *args, **kwargs):
-        pass
-
+    @abstractmethod
     def train(self, *args, **kwargs):
         raise NotImplementedError()
+
+    def get_action(self, observation: "EncodedObservationDict", *args, **kwargs):
+        pass
+
+    @property
+    def observation_space_list(self) -> List["BaseObservationSpace"]:
+        raise NotImplementedError()
+
+    @property
+    def observation_space_kwargs(self) -> Dict[str, Any]:
+        raise NotImplementedError()
+
+    @property
+    def stack_size(self) -> int:
+        return 1
+
+    @property
+    def parameter_number(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def config(self) -> Dict[str, Any]:
+        return {}
