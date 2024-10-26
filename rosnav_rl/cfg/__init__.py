f60db55d2d45efcecae12638644bb356b5f35d92
tuananhroman <anh.maxiking@hotmail.de>
2024-10-27 01:11:53 +0200

    action space cfg

--- rosnav_rl/cfg/__init__.py
+++ rosnav_rl/cfg/__init__.py
@@ -1,10 +1,9 @@
 from .agent import AgentCfg
 from .reward import RewardCfg
 from .stable_baselines3.ppo import (
-    ActionSpaceCfg,
-    CustomDiscreteActionSpaceCfg,
     PPO_Algorithm_Cfg,
     PPO_Policy_Cfg,
 )
 from .framework import FrameworkCfg
 from .stable_baselines3.framework import SB3_Model_Cfg
+from .action_space.action_space import ActionSpaceCfg, DiscreteFromBoxActionSpaceCfg
