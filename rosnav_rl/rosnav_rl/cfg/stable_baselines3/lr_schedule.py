84c43f38526487d83c528f82a4cc6217eb1529c3
tuananhroman <anh.maxiking@hotmail.de>
2024-10-27 01:11:43 +0200

    lr schedule refactor

--- rosnav_rl/rosnav_rl/cfg/stable_baselines3/lr_schedule.py
+++ rosnav_rl/rosnav_rl/cfg/stable_baselines3/lr_schedule.py
@@ -1,26 +1,17 @@
 from typing import Any, Callable, Optional
 
-from pydantic import BaseModel, AfterValidator
-from rosnav_rl.utils.model.sb3.learning_rate_schedules import load_lr_schedule
-from typing_extensions import Annotated
-
-
-def validate_learning_rate_scheduler_type(value):
-    # check if value can be imported from learning_rate_schedules
-    try:
-        load_lr_schedule(value, {})
-    except Exception as e:
-        raise ValueError(f"Invalid learning rate scheduler type: {value}. Error: {e}")
-
-
-SchedulerType = Annotated[str, AfterValidator(validate_learning_rate_scheduler_type)]
+from pydantic import BaseModel, model_validator
+from rosnav_rl.utils.stable_baselines3.model.learning_rate_schedules import (
+    load_lr_schedule,
+)
 
 
 class LearningRateSchedulerCfg(BaseModel):
-    type: SchedulerType = "linear"
+    type: str = "linear"
     kwargs: dict = {"initial_value": 0.001, "final_value": 0.0001}
-    _callable: Optional[Callable[[Any], Callable[[float], float]]] = None
+    callable: Optional[Callable[[Any], Callable[[float], float]]] = None
 
-    def __init__(self, **data):
-        super().__init__(**data)
-        # self._callable = load_lr_schedule(self.type, self.kwargs)
+    @model_validator(mode="after")
+    def load_scheduler(self):
+        self.callable = load_lr_schedule(self.type, self.kwargs)
+        return self
