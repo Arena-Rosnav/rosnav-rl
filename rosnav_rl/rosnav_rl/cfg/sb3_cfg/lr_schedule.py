ea54377d6633a071b390eeb631965ada6228a0b6
tuananhroman <anh.maxiking@hotmail.de>
2024-11-15 16:07:12 +0100

    revert loading callable on lr schedule cfg

--- rosnav_rl/rosnav_rl/cfg/sb3_cfg/lr_schedule.py
+++ rosnav_rl/rosnav_rl/cfg/sb3_cfg/lr_schedule.py
@@ -1,17 +1,6 @@
-from typing import Any, Callable, Optional
-
-from pydantic import BaseModel, model_validator
-from rosnav_rl.utils.stable_baselines3.model.learning_rate_schedules import (
-    load_lr_schedule,
-)
+from pydantic import BaseModel
 
 
 class LearningRateSchedulerCfg(BaseModel):
     type: str = "linear"
     kwargs: dict = {"initial_value": 0.001, "final_value": 0.0001}
-    callable: Optional[Callable[[Any], Callable[[float], float]]] = None
-
-    @model_validator(mode="after")
-    def load_scheduler(self):
-        self.callable = load_lr_schedule(self.type, self.kwargs)
-        return self
