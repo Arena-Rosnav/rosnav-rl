c67337067c02f080d55bbdab7d2e71f0e138ff8f
tuananhroman <anh.maxiking@hotmail.de>
2024-12-12 18:35:27 +0100

    base model

--- rosnav_rl/rosnav_rl/model/model.py
+++ rosnav_rl/rosnav_rl/model/model.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Any, Dict, List
 from pydantic import BaseModel
 
 if TYPE_CHECKING:
+    from rosnav_rl.rl_agent import RL_Agent
     from rosnav_rl.spaces import BaseObservationSpace, EncodedObservationDict
 
 import rosnav_rl.cfg.sb3_cfg as sb3_cfg
@@ -11,15 +12,23 @@ from rosnav_rl.utils.type_aliases import _SupportedRosnavRLModels
 
 
 class RL_Model(ABC):
-    _model = None
-    _algorithm_cfg: BaseModel = None
+    _model: ...
+    _algorithm_cfg: BaseModel
+    _rl_agent: "RL_Agent"
 
-    def __init__(self, algorithm_cfg: BaseModel, *args, **kwargs) -> None:
+    def __init__(
+        self, rl_agent: "RL_Agent", algorithm_cfg: BaseModel, *args, **kwargs
+    ) -> None:
+        self._rl_agent = rl_agent
         self._algorithm_cfg = algorithm_cfg
 
     @abstractmethod
     def setup_model(self, *args, **kwargs):
         pass
+
+    @abstractmethod
+    def train(self, *args, **kwargs):
+        raise NotImplementedError()
 
     @abstractmethod
     def save(self, *args, **kwargs):
@@ -28,10 +37,6 @@ class RL_Model(ABC):
     @abstractmethod
     def load(self, *args, **kwargs):
         pass
-
-    @abstractmethod
-    def train(self, *args, **kwargs):
-        raise NotImplementedError()
 
     def get_action(self, observation: "EncodedObservationDict", *args, **kwargs):
         pass
@@ -54,7 +59,7 @@ class RL_Model(ABC):
         self._model = model
 
     @property
-    def algorithm_cfg(self) -> "sb3_cfg.BaseAlgorithmCfg":
+    def algorithm_cfg(self) -> "sb3_cfg.SBAlgorithmCfg":
         return self._algorithm_cfg
 
     @property
