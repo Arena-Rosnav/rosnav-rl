96b66c6073ee6e15d58415c9af88eddccdd8ddf4
tuananhroman <anh.maxiking@hotmail.de>
2025-01-10 18:14:44 +0100

    name generator

--- rosnav_rl/rosnav_rl/utils/name_generator.py
+++ rosnav_rl/rosnav_rl/utils/name_generator.py
@@ -3,23 +3,29 @@ from typing import TYPE_CHECKING, Literal
 
 import rospy
 
+from rosnav_rl.utils.type_aliases import SupportedRLFrameworks
+
 if TYPE_CHECKING:
     from rosnav_rl.cfg import FrameworkCfg
 
 
-def generate_name(architecture_name: str, robot: str = None) -> str:
+def generate_name(architecture_name: str, algorithm: str, robot: str = None) -> str:
     start_time = dt.now().strftime("%Y_%m_%d__%H_%M_%S")
     robot_model = rospy.get_param("robot_model", "") if robot is None else robot
-    agent_name = f"{robot_model}_{architecture_name}_{start_time}"
+    agent_name = f"{robot_model}_{algorithm}_{architecture_name}_{start_time}"
     return agent_name
 
 
 def generate_sb3_agent_name(framework_cfg: "FrameworkCfg", robot: str = None) -> str:
-    return generate_name(framework_cfg.algorithm.architecture_name, robot=robot)
+    return generate_name(
+        framework_cfg.algorithm.architecture_name,
+        framework_cfg.algorithm.__algorithm_name__,
+        robot=robot,
+    )
 
 
 def generate_agent_name(framework_cfg: "FrameworkCfg", robot: str = None) -> str:
-    if framework_cfg.name == "stable_baselines3":
+    if framework_cfg.__name__ == SupportedRLFrameworks.STABLE_BASELINES3:
         return generate_sb3_agent_name(framework_cfg, robot=robot)
     else:
         raise ValueError(
