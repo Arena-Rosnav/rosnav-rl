924bf3093e56476aed07731cb0d3f029c78386ee
tuananhroman <anh.maxiking@hotmail.de>
2025-01-17 15:58:40 +0100

    lr scheduler loading

--- rosnav_rl/rosnav_rl/utils/stable_baselines3/model/learning_rate_schedules/__init__.py
+++ rosnav_rl/rosnav_rl/utils/stable_baselines3/model/learning_rate_schedules/__init__.py
@@ -1,10 +1,23 @@
-from typing import Callable
+from typing import Callable, Union
+
+from rosnav_rl.cfg.sb3_cfg.lr_schedule import LearningRateSchedulerCfg
 
 from .linear import linear_decay
 from .square_root import square_root_decay
 
 
-def load_lr_schedule(type: str, settings: dict) -> Callable:
+def load_lr_schedule(
+    settings: Union[float, dict, LearningRateSchedulerCfg]
+) -> Callable:
+    if isinstance(settings, LearningRateSchedulerCfg):
+        return _get_lr_schedule(settings.type, settings.kwargs)
+    elif isinstance(settings, dict):
+        return _get_lr_schedule(settings["type"], settings["settings"])
+    elif isinstance(settings, float):
+        return settings
+
+
+def _get_lr_schedule(type: str, settings: dict) -> Callable:
     if type == "linear":
         return linear_decay(**settings)
     elif type == "square_root":
