baecbb3af9204641b00f0e4f591e080866e6c7a1
tuananhroman <anh.maxiking@hotmail.de>
2025-01-17 16:18:23 +0100

    old import

--- rosnav_rl/action_server/arena_server.py
+++ rosnav_rl/action_server/arena_server.py
@@ -66,7 +66,6 @@ class ArenaActionServer(ActionServer):
             ObservationCollector: An instance of ObservationManager configured
             with the required observation units and simulation state container.
         """
-        import task_generator.constants as arena_task_constants
         import task_generator.utils as arena_task_utils
 
         obs_manager = ObservationManager(
