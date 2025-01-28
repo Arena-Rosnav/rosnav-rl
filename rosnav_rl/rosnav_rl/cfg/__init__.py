5d82c21b4a37ad71f559b1c842b610a47a3d6f37
tuananhroman <anh.maxiking@hotmail.de>
2025-01-28 22:47:15 +0100

    init

--- rosnav_rl/rosnav_rl/cfg/__init__.py
+++ rosnav_rl/rosnav_rl/cfg/__init__.py
@@ -1,3 +1,5 @@
+from sb3_cfg import StableBaselinesCfg
+
 from .action_space.action_space import ActionSpaceCfg, DiscreteFromBoxActionSpaceCfg
 from .agent import AgentCfg
 from .framework import FrameworkCfg
