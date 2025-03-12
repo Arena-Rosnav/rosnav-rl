76c7976c61a88b2855ccce6052ad988caf8f4aed
tuananhroman <anh.maxiking@hotmail.de>
2025-03-12 20:11:03 +0100

    Refactor configuration classes for improved structure and clarity; add DiscreteFromBoxActionSpaceCfg class and enhance docstrings across multiple files.

--- rosnav_rl/cfg/__init__.py
+++ rosnav_rl/cfg/__init__.py
@@ -1,7 +1,4 @@
-from .sb3_cfg import StableBaselinesCfg
-from ..model.dreamerv3.cfg import DreamerV3Cfg
-
-from .action_space.action_space import ActionSpaceCfg, DiscreteFromBoxActionSpaceCfg
+from .action_space import ActionSpaceCfg, DiscreteFromBoxActionSpaceCfg
 from .agent import AgentCfg
 from .framework import FrameworkCfg
 from .reward import RewardCfg
