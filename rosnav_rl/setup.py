0d859a74ca293f8a58689287f235ae69b9f7f826
tuananhroman <anh.maxiking@hotmail.de>
2025-03-28 16:49:19 +0100

    upgrade package files to ros2

--- rosnav_rl/setup.py
+++ rosnav_rl/setup.py
@@ -1,9 +1,26 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
+import os
+from glob import glob
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = "rosnav_rl"
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(packages=["rosnav_rl"])
-
-setup(**setup_args)
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        # Remove any duplicate entries for resource files
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Your Name",
+    maintainer_email="your.email@example.com",
+    description="Your package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["your_node = rosnav_rl.your_module:main"],
+    },
+)
